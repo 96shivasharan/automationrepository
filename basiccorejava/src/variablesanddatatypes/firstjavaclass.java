bad9285b2dfaecd2058434e48f70599f81cbda89
shivasharan <dshiva5161@gmail.com>
2019-07-28 11:08:35 +0530

    commiting variables first

--- basiccorejava/src/variablesanddatatypes/firstjavaclass.java
+++ basiccorejava/src/variablesanddatatypes/firstjavaclass.java
@@ -3,9 +3,18 @@ package variablesanddatatypes;
 public class firstjavaclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello selenium");
-
+		//primitive data types& non primitive data types
+		//primitive data typs--,boolean->int,char,doublen
+		//primitive data types-->byte,short,long&float
+		
+		int age=23;  //byte,short, int, long;
+		char gender='f';
+		boolean married=false;
+		double marks=7.8;//float
+		
+	String name="shiva";
+	System.out.println(age +" " +gender+" " +marks);
+		
 	}
 
 }
