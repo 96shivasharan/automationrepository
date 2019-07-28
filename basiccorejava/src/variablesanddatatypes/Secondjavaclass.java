7c415ad62de4886f3433c4e8703e474912c1b8cc
shivasharan <dshiva5161@gmail.com>
2019-07-28 11:09:28 +0530

    commiting secondjava

---
+++ basiccorejava/src/variablesanddatatypes/Secondjavaclass.java
@@ -0,0 +1,17 @@
+package variablesanddatatypes;
+
+public class Secondjavaclass {
+	public static void main(String[] args)
+	{
+		//Arthimatic opertors
+		int a=10;
+		int b=20;
+		// +,-,*,/
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a%b);
+		
+	}
+
+}
